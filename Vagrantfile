c5f5789f21aa207adf4de29d5c9dec788f711227
unknown <lynchthomasj@outlook.com>
2015-02-23 23:55:52 +0000

    Puppet manifest file

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant::Config.run do |config|
 config.vm.box = "lucid32"
 config.vm.forward_port 80, 3000
+config.vm.provision :puppet
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,17 @@
 exec {"apt-get update":
-path=> "usr/bin",
+path => "/usr/bin",
 }
-package {"apache2": ensure => present, require => Exec["apt-get udpate"],}
-service {"apache2": ensure => present, require => Package["apache2"],}
-file {"var/www/project1": ensure => "link", target => Package["apache2"], notify => Service["apache2"],}
+package {"apache2":
+ensure => present,
+ require => Exec["apt-get update"],
+ }
+service {"apache2":
+ ensure => "running",
+ require => Package["apache2"],
+ }
+file {"/var/www/project1":
+ ensure => "link",
+ target => "/vagrant/project1",
+ require => Package["apache2"],
+ notify => Service["apache2"],
+ }
